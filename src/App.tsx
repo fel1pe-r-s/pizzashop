7f4e3499b25ea54cadaee190a3c20ff2e6d98cec
fel1pe-r-s <1felipersilva1@gmail.com>
2024-04-18 20:28:10 -0300

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { router } from './routes'
 import { RouterProvider } from 'react-router-dom'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate='%s | pizza.shop' />
-      <Toaster richColors closeButton />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate='%s | pizza.shop' />
+        <Toaster richColors closeButton />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
+
   )
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { Home, Pizza, Utensils } from "lucide-react";
 import { Separator } from "./ui/separator";
 import { NavLink } from "./nav-link";
+import { ThemeToggle } from "./theme/theme-toggle";
 
 export function Header() {
     return (
@@ -20,6 +21,9 @@ export function Header() {
                         Pedidos
                     </NavLink>
                 </nav>
+                <div className="ml-auto flex items-center gap-2">
+                    <ThemeToggle />
+                </div>
             </div>
         </div>
     )
