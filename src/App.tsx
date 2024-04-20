bf656c537a1a0b83cc2330f1a0ad75f97e557c28
fel1pe-r-s <1felipersilva1@gmail.com>
2024-04-20 17:35:47 -0300

    config api

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import './global.css'
-import { router } from './routes'
-import { RouterProvider } from 'react-router-dom'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
-import { Toaster } from 'sonner'
-import { ThemeProvider } from './components/theme/theme-provider'
+import "./global.css";
+import { router } from "./routes";
+import { RouterProvider } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 export function App() {
   return (
     <HelmetProvider>
       <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
-        <Helmet titleTemplate='%s | pizza.shop' />
+        <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors closeButton />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
-
-  )
+  );
 }
-
