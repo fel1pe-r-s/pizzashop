029b63ce1bd3fc0da9b367850046745b1a45e114
fel1pe-r-s <1felipersilva1@gmail.com>
2024-04-19 23:52:57 -0300

    cards metics orders

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import { SignIn } from "./pages/auth/sign-in";
-import { Dashboard } from "./pages/app/dashboard";
+import { Dashboard } from "./pages/app/dasboard/dashboard";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { SignUp } from "./pages/auth/sign-up";
